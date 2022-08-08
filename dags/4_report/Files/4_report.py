1b04b8c5f222447788ae81ba848b72b929d65020
Ragnarok-039 <brezhnev.aleksandr@gmail.com>
2022-08-08 17:37:29 +0300

    4 отчет

--- dags/4_report/Files/4_report.py
+++ dags/4_report/Files/4_report.py
@@ -29,7 +29,26 @@ dag = DAG(
 )
 
 
-path_to_file_airflow = '/root/airflow/dags/10_report/Files/'
-path_to_file_mysql = '/home/glotov/84.201.164.249/10_report/'
+path_to_file_airflow = '/root/airflow/dags/4_report/Files/'
+path_to_file_mysql = '/home/glotov/84.201.164.249/4_report/'
 path_to_file_dbs = '/4_report/Files/'
 
+# Блок предварительного удаления файлов с сервера.
+main_del = PythonOperator(
+    task_id='main_del', 
+    python_callable=del_file, 
+    op_kwargs={'from_path': path_to_file_mysql, 'file': 'Main.csv', 'db': 'Server_MySQL'}, 
+    dag=dag
+    )
+working_time_del = PythonOperator(
+    task_id='working_time_del', 
+    python_callable=del_file, 
+    op_kwargs={'from_path': path_to_file_mysql, 'file': 'Working_time.csv', 'db': 'Server_MySQL'}, 
+    dag=dag
+    )
+users_total_del = PythonOperator(
+    task_id='users_total_del', 
+    python_callable=del_file, 
+    op_kwargs={'from_path': path_to_file_mysql, 'file': 'Users_total.csv', 'db': 'Server_MySQL'}, 
+    dag=dag
+    )
