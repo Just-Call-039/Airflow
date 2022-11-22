4941a8a670fed8be01c43cee013be9f4af36391f
Ragnarok-039 <brezhnev.aleksandr@gmail.com>
2022-11-22 10:30:19 +0300

    4_report_current_month

--- dags/4_report/4_report_current_month.py
+++ dags/4_report/4_report_current_month.py
@@ -26,21 +26,21 @@ default_args = {
     }
 
 dag = DAG(
-    dag_id='4_report_previous_month',
+    dag_id='4_report_current_month',
     schedule_interval='0 9-15/2 * * *',
     default_args=default_args
     )
 
 
 path_to_file_airflow = '/root/airflow/dags/4_report/Files/'
-path_airflow_main_folder = '/root/airflow/dags/4_report/Files/main_folder/'
-path_airflow_requests_folder = '/root/airflow/dags/4_report/Files/requests_folder/'
-path_airflow_working_time_folder = '/root/airflow/dags/4_report/Files/working_time_folder/'
+path_airflow_main_folder = f'{path_to_file_airflow}main_folder/'
+path_airflow_requests_folder = f'{path_to_file_airflow}requests_folder/'
+path_airflow_working_time_folder = f'{path_to_file_airflow}working_time_folder/'
 
 path_to_file_mysql = '/home/glotov/192.168.1.117/4_report/'
-path_mysql_main_folder = '/home/glotov/192.168.1.117/4_report/main_folder/'
-path_mysql_requests_folder = '/home/glotov/192.168.1.117/4_report/requests_folder/'
-path_mysql_working_time_folder = '/home/glotov/192.168.1.117/4_report/working_time_folder/'
+path_mysql_main_folder = f'{path_to_file_mysql}main_folder/'
+path_mysql_requests_folder = f'{path_to_file_mysql}requests_folder/'
+path_mysql_working_time_folder = f'{path_to_file_mysql}working_time_folder/'
 
 path_to_file_dbs = '/4_report/Files/'
 path_dbs_main_folder = '/4_report/Files/main_folder/'
