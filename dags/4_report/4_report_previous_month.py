efd1ba4c967dd6726a83dabb8cc659192587ab46
Ragnarok-039 <brezhnev.aleksandr@gmail.com>
2022-11-23 12:53:16 +0300

    Завершил 4_report_previous_month.

--- dags/4_report/4_report_previous_month.py
+++ dags/4_report/4_report_previous_month.py
@@ -46,10 +46,9 @@ path_dbs_requests_folder = f'{path_to_file_dbs}requests_folder/'
 path_dbs_working_time_folder = f'{path_to_file_dbs}working_time_folder/'
 
 cloud_name = 'cloud_117'
-sql_main = '/root/airflow/dags/4_report/SQL/main_current_month.sql'
-sql_requests = '/root/airflow/dags/4_report/SQL/requests_current_month.sql'
-sql_working_time = '/root/airflow/dags/4_report/SQL/working_time_current_month.sql'
-sql_users = '/root/airflow/dags/4_report/SQL/Users_total.sql'
+sql_main = '/root/airflow/dags/4_report/SQL/main_previous_month.sql'
+sql_requests = '/root/airflow/dags/4_report/SQL/requests_previous_month.sql'
+sql_working_time = '/root/airflow/dags/4_report/SQL/working_time_previous_month.sql'
 
 today = datetime.date.today()
 previous_date = today - dateutil.relativedelta.relativedelta(months=1)
@@ -61,25 +60,19 @@ file_name = f'{year}_{month}.csv'
 main_folder_sql = PythonOperator(
     task_id='main_folder_sql', 
     python_callable=sql_query_to_csv, 
-    op_kwargs={'cloud': cloud_name, 'path_sql_file': sql_main, 'path_csv_file': path_airflow_main_folder, 'name_csv_file': 'main_current_month.csv'}, 
+    op_kwargs={'cloud': cloud_name, 'path_sql_file': sql_main, 'path_csv_file': path_airflow_main_folder, 'name_csv_file': file_name}, 
     dag=dag
     )
 requests_folder_sql = PythonOperator(
     task_id='requests_folder_sql', 
     python_callable=sql_query_to_csv, 
-    op_kwargs={'cloud': cloud_name, 'path_sql_file': sql_requests, 'path_csv_file': path_airflow_requests_folder, 'name_csv_file': 'requests_current_month.csv'}, 
+    op_kwargs={'cloud': cloud_name, 'path_sql_file': sql_requests, 'path_csv_file': path_airflow_requests_folder, 'name_csv_file': file_name}, 
     dag=dag
     )
 working_time_folder_sql = PythonOperator(
     task_id='working_time_folder_sql', 
     python_callable=sql_query_to_csv, 
-    op_kwargs={'cloud': cloud_name, 'path_sql_file': sql_working_time, 'path_csv_file': path_airflow_working_time_folder, 'name_csv_file': 'working_time_current_month.csv'}, 
-    dag=dag
-    )
-users_total_sql = PythonOperator(
-    task_id='users_total_sql', 
-    python_callable=sql_query_to_csv, 
-    op_kwargs={'cloud': cloud_name, 'path_sql_file': sql_users, 'path_csv_file': path_to_file_airflow, 'name_csv_file': 'Users_total.csv'}, 
+    op_kwargs={'cloud': cloud_name, 'path_sql_file': sql_working_time, 'path_csv_file': path_airflow_working_time_folder, 'name_csv_file': file_name}, 
     dag=dag
     )
 
@@ -87,25 +80,19 @@ users_total_sql = PythonOperator(
 main_transfer_to_dbs = PythonOperator(
     task_id='main_transfer_to_dbs', 
     python_callable=transfer_file_to_dbs, 
-    op_kwargs={'from_path': path_airflow_main_folder, 'to_path': path_dbs_main_folder, 'file': 'main_current_month.csv', 'db': 'DBS'}, 
+    op_kwargs={'from_path': path_airflow_main_folder, 'to_path': path_dbs_main_folder, 'file': file_name, 'db': 'DBS'}, 
     dag=dag
     )
 requests_transfer_to_dbs = PythonOperator(
     task_id='requests_transfer_to_dbs', 
     python_callable=transfer_file_to_dbs, 
-    op_kwargs={'from_path': path_airflow_requests_folder, 'to_path': path_dbs_requests_folder, 'file': 'requests_current_month.csv', 'db': 'DBS'}, 
+    op_kwargs={'from_path': path_airflow_requests_folder, 'to_path': path_dbs_requests_folder, 'file': file_name, 'db': 'DBS'}, 
     dag=dag
     )
 working_transfer_to_dbs = PythonOperator(
     task_id='working_transfer_to_dbs', 
     python_callable=transfer_file_to_dbs, 
-    op_kwargs={'from_path': path_airflow_working_time_folder, 'to_path': path_dbs_working_time_folder, 'file': 'working_time_current_month.csv', 'db': 'DBS'}, 
-    dag=dag
-    )
-users_total_transfer_to_dbs = PythonOperator(
-    task_id='users_total_transfer_to_dbs', 
-    python_callable=transfer_file_to_dbs, 
-    op_kwargs={'from_path': path_to_file_airflow, 'to_path': path_to_file_dbs, 'file': 'Users_total.csv', 'db': 'DBS'}, 
+    op_kwargs={'from_path': path_airflow_working_time_folder, 'to_path': path_dbs_working_time_folder, 'file': file_name, 'db': 'DBS'}, 
     dag=dag
     )
 
@@ -125,4 +112,3 @@ send_telegram_message = TelegramOperator(
 main_folder_sql >> main_transfer_to_dbs >> send_telegram_message
 requests_folder_sql >> requests_transfer_to_dbs >> send_telegram_message
 working_time_folder_sql >> working_transfer_to_dbs >> send_telegram_message
-users_total_sql >> users_total_transfer_to_dbs >> send_telegram_message
