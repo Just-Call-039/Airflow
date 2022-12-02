68a0eda65c1d60288f7a10c2ad8f2c7bf5b6cbdd
Ragnarok-039 <brezhnev.aleksandr@gmail.com>
2022-12-02 09:13:49 +0300

    Изменения 4 отчета. SQL и время работы пред месяца

--- dags/4_report/4_report_previous_month.py
+++ dags/4_report/4_report_previous_month.py
@@ -25,7 +25,7 @@ default_args = {
 
 dag = DAG(
     dag_id='4_report_previous_month',
-    schedule_interval='30 7-14 1-3 * 1,4',
+    schedule_interval='30 7-14 1-3,4-30/2 * *',
     default_args=default_args
     )
 
